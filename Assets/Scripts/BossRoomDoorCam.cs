e2e152095a0cfe824f148e6f28e70eca54000ec4
EthanCrisford <114390612+EthanCrisford@users.noreply.github.com>
2023-05-19 12:19:54 +0100

    1

---
+++ Assets/Scripts/BossRoomDoorCam.cs
@@ -0,0 +1,30 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossRoomDoorCam : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D (Collider2D collision)
+    {
+
+        var camera = Camera.main;
+        var brain = (camera == null) ? null : camera.GetComponent<CinemachineBrain>();
+        var vcam = (brain == null) ? null : brain.ActiveVirtualCamera as CinemachineVirtualCamera;
+        if (vcam != null)
+            vcam.m_Lens.OrthographicSize = 50;
+
+
+    }
+
+}
