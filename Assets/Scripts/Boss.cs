0775f478664b31ce857756040d1944f60360d9b3
EthanCrisford <476052@chrome.escg.ac.uk>
2023-05-24 12:07:23 +0100

    1

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
 public class Boss : MonoBehaviour
 {
     public Slider slider;
 
+    private static int damage;
+
+    private static int maxHealth = 20;
+    private static int health;
+
+    void Start()
+    {
+        SetMaxHealth(maxHealth);
+        SetHealth(health);
+        damage = 1;
+    }
+
     public void SetMaxHealth(int health)
     {
-        slider.maxValue = health;
+        slider.maxValue = 20;
         slider.value = health;
     }
 
     public void SetHealth(int health)
     {
         slider.value = health;
+    }
+
+    public static void DamageBoss(int damage)
+    {
+        health -= damage;
     }
 }
